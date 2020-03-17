7d4a7a9d8347cb1e3d540280c60c9eca25469e94
Clythez <Clythez@gmail.com>
2020-03-17 12:26:19 +0100

    Add files via upload

---
+++ Week11/src/main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.intro();
+
+        menu.ratingsDesc();
+        // menu.ratingsAsc();
+        // menu.searchForActor();
+        // menu.print4();
+        // System.out.println(print4.get(484).toString());
+        // menu.writeToCSV();
+    }
+}
